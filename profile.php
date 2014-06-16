a854e4145c915e564243f60ec7f647172b6e54de
Andrey Taradiuk <andreyt007@gmail.com>
2014-06-16 22:51:52 +0300

    Стажировка

---
+++ profile.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include_once "back-end/config/init.php";
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="windows-1251">
+	<link href="front-end/css/style.css" rel="stylesheet">
+    <link href="front-end/css/page_nav.css" rel="stylesheet">
+	<title>Футбол + | Профиль пользователя</title>
+</head>
+
+<body>
+
+	<div id="wrap">
+    	<?php include_once "header.php"; ?>
+        
+        <div id="inner_wrap">
+        	<?php include_once "left_sidebar.php"; ?>
+            
+            <div id="content">
+            	<?php
+				
+				?>
+            </div>
+            
+            <?php include_once "right_sidebar.php"; ?>
+        </div>
+    </div>
+
+</body>
+</html>
